8f33d51a7047e1dec80eb296b2db650bd8a86aef
Roman Yaroshenko <roma.yaroshenko@gmail.com>
2021-07-05 08:57:03 +0300

    fixed mistakes in foreign script

--- db/04_constr_foreign.sql
+++ db/04_constr_foreign.sql
@@ -1,4 +1,4 @@
 use usersdb;
 
-alter table `users` add constraint `users_groups`   foreign key (`id_groups`)   referemces `groups`(`id`);
-alter table `users` add constraint `users_statuses` foreign key (`id_statuses`) referemces `statuses`(`id`);
+alter table `users` add constraint `users_groups`   foreign key (`id_groups`)   references `groups`(`id`);
+alter table `users` add constraint `users_statuses` foreign key (`id_statuses`) references `statuses`(`id`);
--- db/usersdb.sql
+++ db/usersdb.sql
@@ -1,3 +1,4 @@
 source 01_tables.sql;
 source 02_constr_unique.sql;
 source 03_data_init.sql;
+source 04_constr_foreign.sql;
